3c285f20bfd18c01e83c6a0bce92dc772687316d
Akira Taguchi <akira.taguchi@napa.fi>
2022-04-25 16:25:33 +0300

    Complete Getting Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MyCounterComponent } from './my-counter/my-counter-component';
+
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  declarations: [AppComponent, MyCounterComponent],
   bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule {}
+export class AppModule { }
 
 
 /*
---
+++ src/app/my-counter/my-counter-component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { increment, decrement, reset } from '../counter.actions';
+
+@Component({
+    selector: 'app-my-counter',
+    templateUrl: './my-counter.component.html',
+})
+export class MyCounterComponent {
+    count$: Observable<number>
+
+    constructor(private store: Store<{ count: number }>) {
+        this.count$ = store.select('count');
+        // TODO: Connect `this.count$` stream to the current store `count` state
+    }
+
+    increment() {
+        this.store.dispatch(increment());
+        // TODO: Dispatch an increment action
+    }
+
+    decrement() {
+        this.store.dispatch(decrement());
+        // TODO: Dispatch a decrement action
+    }
+
+    reset() {
+        this.store.dispatch(reset());
+        // TODO: Dispatch a reset action
+    }
+}
